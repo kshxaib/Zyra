9d96bcfe52b8d898502742564972b07d06d34ceb
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-08-20 15:28:25 +0530

    intial project setup

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+
+
+const app = express()
+
+dotenv.config()
+const PORT = process.env.PORT || 8000
+
+app.get("/heathcheck", (req, res) => {
+    res.send("Healthy")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
